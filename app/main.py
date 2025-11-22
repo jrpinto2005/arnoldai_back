6d13384133aae064cbe9bf5724bfb38113ec9d49
Julian Pinto <jr.pinto@uniandes.edu.co>
2025-11-22 12:54:11 -0500

    Working chatbot Working voice

--- app/main.py
+++ app/main.py
@@ -5,14 +5,29 @@ from fastapi.staticfiles import StaticFiles
 from app.core.config import settings
 from app.db.session import Base, engine
 from app.api.routes import chat, sessions, setup, tts
+from fastapi.middleware.cors import CORSMiddleware
 
 # 👉 Esto crea todas las tablas en la BD
 Base.metadata.create_all(bind=engine)
 
 # Crear carpeta media si no existe
 os.makedirs(settings.MEDIA_DIR, exist_ok=True)
+origins = [
+    "http://localhost:19006",  # Expo web
+    "http://localhost:8081",
+    "http://127.0.0.1:19006",
+    # añade aquí la IP de tu máquina en la red local si pruebas en dispositivo físico
+    "*"
+]
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
